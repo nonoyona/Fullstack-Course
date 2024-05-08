ca3ca70a1551459e51e9f369db2088743ccae994
Noah- Yannick Schmid <44346907+nonoyona@users.noreply.github.com>
2024-05-08 17:44:17 +0300

    1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,14 +4,27 @@ const Button = ({ handleClick, text }) => (
 	<button onClick={handleClick}>{text}</button>
 );
 
+const Statistics = ({ good, neutral, bad }) => {
+	const total = good + neutral + bad;
+	const average = (good - bad) / total;
+	return (
+		<>
+			<h1>statistics</h1>
+			<div>good {good}</div>
+			<div>neutral {neutral}</div>
+			<div>bad {bad}</div>
+			<div>all {total}</div>
+			<div>average {average.toFixed(4)}</div>
+			<div>positive {((good / total) * 100).toFixed(4)} %</div>
+		</>
+	);
+};
+
 const App = () => {
 	// save clicks of each button to its own state
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
-
-	const total = good + neutral + bad;
-	const average = (good - bad) / total;
 
 	return (
 		<div>
@@ -19,13 +32,7 @@ const App = () => {
 			<Button handleClick={() => setGood(good + 1)} text="good" />
 			<Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
 			<Button handleClick={() => setBad(bad + 1)} text="bad" />
-			<h1>statistics</h1>
-			<div>good {good}</div>
-			<div>neutral {neutral}</div>
-			<div>bad {bad}</div>
-      <div>all {total}</div>
-			<div>average {average.toFixed(4)}</div>
-      <div>positive {((good / total) * 100).toFixed(4)} %</div>
+			<Statistics good={good} neutral={neutral} bad={bad} />
 		</div>
 	);
 };
